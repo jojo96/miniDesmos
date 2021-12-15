e82937ce59d2cebe9ef94f9266aca5c6868d10af
Ujjayanta <aprajit09041996@gmail.com>
2021-12-15 11:16:04 +0100

    Update miniDesmos.py

--- miniDesmos.py
+++ miniDesmos.py
@@ -9,107 +9,139 @@ import random
 import warnings
 warnings.filterwarnings("ignore")
 
-def chunks(lst, n):
-    """Yield successive n-sized chunks from lst."""
-    for i in range(0, len(lst), n):
-        yield lst[i:i + n]
-
-def plotter(img):
-    strs = []
-    pts = []
-    for ele in res:
-        x = []
-        y = []
-        for k in ele:
-            x+=k[0][0]
-            y+=k[0][1]
-        if len(x)%2 == 1:
-            x.pop()
-        if len(y)%2 == 1:
-            y.pop() 
-        g = list(chunks(x,2))
-        h = list(chunks(y,2))
-        pts.append(tuple(x))
-        pts.append(tuple(y))
-        pts.append(random.choice(['r','g','b']))
-        pts.append
-        for i in range(len(g)):
-            try:
-                m, b = np.polyfit(g[i], h[i], 1)
-                st = 'y ='+str(m)+'*x+'+str(b)+'{'+str(min(h[i]))+'<y<'+str(max(h[i]))+'}'+'{'+str(min(g[i]))+'<x<'+str(max(g[i]))+'}'
-                strs.append(st)
-            except:
-                continue
-    return strs, pts
 
 st.header("Generate plots for anything")
-st.write("Choose any image and get corresponding equations for plotting the image. This is inspired by www.desmos.com")
+st.write("Choose any image and get corresponding equations for plotting the image. This is inspired by www.desmos.com. Examples below:")
 
-img = st.file_uploader("Choose an image...")
+#img = st.file_uploader("Choose an image...")
 #st.write(type(img))
-strs = []
-pts = []
-res = []
-s = 0
-x = []
-y = []
 
 
-if st.button('Generate plot'):
-    #img = cv.imread(img)
-    img = Image.open(img)
-    img = np.array(img)
-    img = cv.flip(img, 0)
-    imgray = cv.cvtColor(img, cv.COLOR_BGR2GRAY)
-    edged = cv.Canny(imgray, 40, 200)
-    ret, thresh = cv.threshold(edged, 50, 255, 0)
-    contours, hierarchy = cv.findContours(thresh, cv.RETR_TREE, cv.CHAIN_APPROX_SIMPLE)
-    
-    
-    r = cv.drawContours(img, contours, -1, (0,255,0), 3)
-    #cv.imshow('img',r)
-    #cv.waitKey(0)
-    
-    st.image(r)
-    
-    for i in contours:
-        if len(i)>3:
-            res.append(i)
-            s+=len(i)
-    r = cv.drawContours(img, res, -1, (0,255,0), 3)
-    for ele in res:
+
+Options = ["Rayquaza","Abra"]
+choose = st.sidebar.selectbox("Select your Pokemon:", Options)
+
+if st.button('Plot Pokemon'):
+    if choose == 'Rayquaza':
+        image = Image.open(r'ray_.png')
+        st.image(image, caption = 'Rayquaza plot')
         
-        for k in ele:
-            x+=k[0][0]
-            y+=k[0][1]
-        if len(x)%2 == 1:
-            x.pop()
-        if len(y)%2 == 1:
-            y.pop() 
-        g = list(chunks(x,2))
-        h = list(chunks(y,2))
-        pts+=tuple(x)
-        pts+=tuple(y)
-        pts.append(random.choice(['r','g','b']))
-        pts.append
-        for i in range(len(g)):
-            try:
-                m, b = np.polyfit(g[i], h[i], 1)
-                st = 'y ='+str(m)+'*x+'+str(b)+'{'+str(min(h[i]))+'<y<'+str(max(h[i]))+'}'+'{'+str(min(g[i]))+'<x<'+str(max(g[i]))+'}'
-                strs.append(st)
-                x.clear()
-                y.clear()
-            except:
-                x.clear()
-                y.clear()
-                continue
-    #st.pyplot.figure(figsize=(10, 10))
-    #st.pyplot.grid()
-    
-st.write(pts)    
-if st.button('gen'):    
-    st.pyplot(*pts);
-    
-    
-   
-   
+        st.write('Equations used to plot Rayquaza (Showing only first 20 here)')
+        
+        img = cv.imread(r"ray.png")
+        img = cv.flip(img, 0)
+        imgray = cv.cvtColor(img, cv.COLOR_BGR2GRAY)
+        edged = cv.Canny(imgray, 40, 200)
+        ret, thresh = cv.threshold(edged, 50, 255, 0)
+        contours, hierarchy = cv.findContours(thresh, cv.RETR_TREE, cv.CHAIN_APPROX_SIMPLE)
+        r = cv.drawContours(img, contours, -1, (0,255,0), 3)
+        
+        res = []
+        s = 0
+        for i in contours:
+            if len(i)>3:
+                res.append(i)
+                s+=len(i)
+        r = cv.drawContours(img, res, -1, (0,255,0), 3)
+        
+        def chunks(lst, n):
+            """Yield successive n-sized chunks from lst."""
+            for i in range(0, len(lst), n):
+                yield lst[i:i + n]
+        
+        
+        strs = []
+        pts = []
+        mb = []
+        for ele in res:
+            x = []
+            y = []
+            for k in ele:
+                x.append(k[0][0])
+                y.append(k[0][1])
+            if len(x)%2 == 1:
+                x.pop()
+            if len(y)%2 == 1:
+                y.pop() 
+            g = list(chunks(x,2))
+            h = list(chunks(y,2))
+            pts.append(tuple(x))
+            pts.append(tuple(y))
+            pts.append(random.choice(['r','g','b']))
+            #pts.append
+            for i in range(len(g)):
+                try:
+                    m, b = np.polyfit(g[i], h[i], 1)
+                    stk = 'y ='+str(m)+'*x+'+str(b)+'{'+str(min(h[i]))+'<y<'+str(max(h[i]))+'}'+'{'+str(min(g[i]))+'<x<'+str(max(g[i]))+'}'
+                    strs.append(stk)
+                    mb.append([m, b, min(g[i]), max(g[i])])
+                except:
+                    continue
+                    
+        st.write(strs[:20]) 
+
+
+    if choose == 'Abra':
+        image = Image.open(r'abra_.png')
+        st.image(image, caption = 'Abra plot')
+        st.write('Equations used to plot Abra (Showing only first 20 here)')
+        
+        img = cv.imread(r"abra.jpg")
+        img = cv.flip(img, 0)
+        imgray = cv.cvtColor(img, cv.COLOR_BGR2GRAY)
+        edged = cv.Canny(imgray, 40, 200)
+        ret, thresh = cv.threshold(edged, 50, 255, 0)
+        contours, hierarchy = cv.findContours(thresh, cv.RETR_TREE, cv.CHAIN_APPROX_SIMPLE)
+        r = cv.drawContours(img, contours, -1, (0,255,0), 3)
+        
+        res = []
+        s = 0
+        for i in contours:
+            if len(i)>3:
+                res.append(i)
+                s+=len(i)
+        r = cv.drawContours(img, res, -1, (0,255,0), 3)
+        
+        def chunks(lst, n):
+            """Yield successive n-sized chunks from lst."""
+            for i in range(0, len(lst), n):
+                yield lst[i:i + n]
+        
+        
+        strs = []
+        pts = []
+        mb = []
+        for ele in res:
+            x = []
+            y = []
+            for k in ele:
+                x.append(k[0][0])
+                y.append(k[0][1])
+            if len(x)%2 == 1:
+                x.pop()
+            if len(y)%2 == 1:
+                y.pop() 
+            g = list(chunks(x,2))
+            h = list(chunks(y,2))
+            pts.append(tuple(x))
+            pts.append(tuple(y))
+            pts.append(random.choice(['r','g','b']))
+            #pts.append
+            for i in range(len(g)):
+                try:
+                    m, b = np.polyfit(g[i], h[i], 1)
+                    stk = 'y ='+str(m)+'*x+'+str(b)+'{'+str(min(h[i]))+'<y<'+str(max(h[i]))+'}'+'{'+str(min(g[i]))+'<x<'+str(max(g[i]))+'}'
+                    strs.append(stk)
+                    mb.append([m, b, min(g[i]), max(g[i])])
+                except:
+                    continue
+                    
+        st.write(strs[:20]) 
+        
+
+st.header('Inspiration')        
+st.write("[Desmos](https://www.desmos.com/calculator) is a graphing calculator that people sometimes misuse for creating amazing graphics.")
+st.write('I had created some graphs before :) [Roger Federer](https://www.desmos.com/calculator/fsxlbhofbn), [Pikachu](https://www.desmos.com/calculator/awkmy5vszg), [Deoxys](https://www.desmos.com/calculator/n66uv6t5wo)')
+st.write('For creating these graphs, I had used simple tools mostly like ellipses, parabolas, circles, and straight lines. Believe me! It is a lot of fun. But, it takes a lot of time too.')
+st.write('So, I was thinking of automating it for a long time. I came across this inspirational video on Youtube that is a full fledged Desmos graph generator which uses Bezier curves to produce amazing Graphics. The video: [How I animate stuff on Desmos Graphing Calculator](https://www.youtube.com/watch?v=BQvBq3K50u8). You can see the equations on the left and the plots as well')
+st.write('For this project, I am aiming for something simple though. I want to plot an image just using straight lines and produce those equations which can be imported into desmos. The code is explained in my medium article. Thank you for visiting :wave:')
